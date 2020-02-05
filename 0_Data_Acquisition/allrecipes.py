2a7ea672a18da57d8d8a4d0459c729a76d3f1978
kaixinwang <kaixinwang@uchicago.edu>
2020-02-05 14:31:15 -0600

    Write some functions for crawling allrecipes

--- 0_Data_Acquisition/allrecipes.py
+++ 0_Data_Acquisition/allrecipes.py
@@ -9,6 +9,41 @@ import string
 import util
 
 
+def get_categories(pm, url="https://www.allrecipes.com/recipes/"):
+    '''
+    Get all categories in allrecipes.
+    '''
+    soup = util.get_soup(url, pm)
+    categories = soup.find_all("div", class_="all-categories-col")
+    dic = dict()
+    for cat in categories:
+        keys = cat.find_all("h3", class_="heading__h3")
+        links_lst = cat.find_all("ul")
+        for i, key in enumerate(keys):
+            key = key.text
+            print(key)
+            dic[key] = dict()
+            links = links_lst[i].find_all("a")
+            for link in links:
+                dic[key][link.text] = link['href']
+
+    return dic
+
+
+def get_recipe_links(pm, url):
+    soup = util.get_soup(url, pm)
+    links = soup.find_all("a")
+    recipe_links = set()
+    for link in links:
+        try:
+            link = link["href"]
+        except KeyError:
+            continue
+        if link[:33] == "https://www.allrecipes.com/recipe" and link[33] != "s":
+            recipe_links.add(link)
+    return recipe_links
+
+
 def crawl(pm, filename):
     '''
     Crawl https://www.foodnetwork.com/
@@ -20,6 +55,7 @@ def crawl(pm, filename):
         json file
     '''    
     return None
+
 
 
 if __name__ == "__main__":
