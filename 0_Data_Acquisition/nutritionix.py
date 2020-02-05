9d9efa578f907e1216e4868c0ce2f561c4f22408
kaixinwang <kaixinwang@uchicago.edu>
2020-02-05 17:52:59 -0600

    Implement get_nutrient function in nutritionix.py

--- 0_Data_Acquisition/nutritionix.py
+++ 0_Data_Acquisition/nutritionix.py
@@ -1,15 +1,33 @@
 '''
-LINK: https://developer.nutritionix.com/admin/access_details
-PYTHON WRAPPER: https://github.com/leetrout/python-nutritionix
+Python Wrapper fo Nutritionix
+Link: https://developer.nutritionix.com/admin/access_details
+Doc: https://trackapi.nutritionix.com/docs/
+Reference: https://github.com/leetrout/python-nutritionix
 '''
 
+import requests
 
-APPLICATION_ID = '95dfa943'
-APPLICATION_KEY = '59e2efc1a67da04885a07f8ea16d5114'
-
-STRING = curl -XPOST https://api.nutritionix.com/v1_1/search -H 'Content-Type: application/json' -d'
-{
- "appId":"95dfa943",
- "appKey":"59e2efc1a67da04885a07f8ea16d5114",
- "query":"Cookies `n Cream"
+URL = "https://trackapi.nutritionix.com/v2/natural/nutrients"
+APP_ID = '95dfa943'
+APP_KEY = '59e2efc1a67da04885a07f8ea16d5114'
+USER_ID = '0'
+HEADERS = {
+    "Content-Type": "application/json",
+    "x-app-id": APP_ID, 
+    "x-app-key": APP_KEY,
+    "x-remote-user-id": USER_ID
 }
+
+def get_nutrient(ingredient):
+    '''
+    Get nutrient informatino of an ingredient
+
+    Inputs:
+        ingredient (string)
+
+    Outputs:
+        dictionary
+    '''
+    data = '{"query": "' + ingredient + '", "locale": "en_US"}'
+    response = requests.post(URL, headers=headers, data=data)
+    return response.json()
