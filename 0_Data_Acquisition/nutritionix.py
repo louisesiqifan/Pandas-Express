be3864fa6fe56000553c26214df3a9466af192f5
Louise Fan <louisesiqifan@gmail.com>
2020-02-05 17:15:24 -0600

    added python wrapper link

--- 0_Data_Acquisition/nutritionix.py
+++ 0_Data_Acquisition/nutritionix.py
@@ -1,7 +1,15 @@
 '''
 LINK: https://developer.nutritionix.com/admin/access_details
+PYTHON WRAPPER: https://github.com/leetrout/python-nutritionix
 '''
 
 
 APPLICATION_ID = '95dfa943'
 APPLICATION_KEY = '59e2efc1a67da04885a07f8ea16d5114'
+
+STRING = curl -XPOST https://api.nutritionix.com/v1_1/search -H 'Content-Type: application/json' -d'
+{
+ "appId":"95dfa943",
+ "appKey":"59e2efc1a67da04885a07f8ea16d5114",
+ "query":"Cookies `n Cream"
+}
