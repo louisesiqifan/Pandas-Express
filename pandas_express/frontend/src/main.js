89aa71b280154226786ff7842c66131a825a33a7
kaixinwang <kaixinwang@uchicago.edu>
2020-02-08 11:14:38 -0600

    Setup Home.vue

--- pandas_express/frontend/src/main.js
+++ pandas_express/frontend/src/main.js
@@ -1,10 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import vuetify from './plugins/vuetify';
+import Home from './components/Home.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.component('home', Home);
 
 new Vue({
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
