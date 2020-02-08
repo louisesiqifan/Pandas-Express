ee832097e82c637b8bd1b49816db7232f437c187
kaixinwang <kaixinwang@uchicago.edu>
2020-02-07 19:01:27 -0600

    Add DATABASE_FILENAME

---
+++ 1_Data_Analysis/access_data.py
@@ -0,0 +1,18 @@
+import sqlite3
+import os
+
+DATABASE_FILENAME = '../0_Data_Acquisition/food_map.db'
+
+def search_by_category(category):
+    db = sqlite3.connect(DATABASE_FILENAME)
+    c = db.cursor()
+    query = '''
+    SELECT recipes.name 
+    FROM recipes JOIN recipe_categories ON recipe_categories.id = recipes.id
+    WHERE recipe_categories.category = ?
+    '''
+    params = (category, )
+    c.execute(query, params)
+    results = c.fetchall()
+    db.close()
+    return results
