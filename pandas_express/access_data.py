ec919e0a9e03082571bbc36c7b9606260dc20b10
Louise Fan <louisesiqifan@gmail.com>
2020-02-08 19:12:15 -0600

    added multi-matching feature to search_by_categories

--- pandas_express/access_data.py
+++ pandas_express/access_data.py
@@ -18,17 +18,29 @@ INDEX_IGNORE = ['the', 'and', 'to', 'with', 'in', 'of', 'until',
                 'spoon', 'be', 'pot', 'more', 'lightly', 'pour']
 
 
-def search_by_category(category):
-    category = category.capitalize()
+def search_by_category(categories):
+    '''
+    To search by categories
+
+    Input:
+        categories(list)
+    Return:
+        results(list)
+    '''
     db = sqlite3.connect(DATABASE_FILENAME)
     c = db.cursor()
-    query = '''
-    SELECT recipes.id
-    FROM recipes JOIN recipe_categories ON recipe_categories.id = recipes.id
-    WHERE recipe_categories.category = ?
+    args = tuple([x.capitalize() for x in categories if x.lower() not in INDEX_IGNORE])
+    sub_q = ','.join(['?']*len(args))
+    query = f'''
+    SELECT r.id, ROUND(k.count*1.0/{len(args)},2) AS score
+    FROM recipes AS r
+    JOIN (SELECT id, COUNT(id) AS count
+          FROM recipe_categories
+          WHERE category IN ({sub_q})
+          GROUP BY id) AS k
+          ON k.id = r.id
     '''
-    params = (category, )
-    c.execute(query, params)
+    c.execute(query, args)
     results = c.fetchall()
     db.close()
     return results
