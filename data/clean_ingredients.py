148532bb9152e80473c1ee27315c9e29f46954a1
Louise Fan <louisesiqifan@gmail.com>
2020-02-12 14:31:40 -0600

    edited clean_ingredients.py and cleaned_ingredients.csv

--- data/clean_ingredients.py
+++ data/clean_ingredients.py
@@ -6,18 +6,6 @@ from fractions import Fraction
 from word2number import w2n
 
 NLP = spacy.load('en_core_web_sm')
-UNITS1 = {"cup": ["cups", "cup", "c.", "c"], "fluid_ounce": ["fl. oz.", "fl oz", "fluid ounce", "fluid ounces"],
-         "gallon": ["gal", "gal.", "gallon", "gallons"], "ounce": ["oz", "oz.", "ounce", "ounces"],
-         "pint": ["pt", "pt.", "pint", "pints"], "pound": ["lb", "lb.", "pound", "pounds"],
-         "quart": ["qt", "qt.", "qts", "qts.", "quart", "quarts"],
-         "tablespoon": ["tbsp.", "tbsp", "T", "T.", "tablespoon", "tablespoons", "tbs.", "tbs"],
-         "teaspoon": ["tsp.", "tsp", "t", "t.", "teaspoon", "teaspoons"],
-         "gram": ["g", "g.", "gr", "gr.", "gram", "grams"], "kilogram": ["kg", "kg.", "kilogram", "kilograms"],
-         "liter": ["l", "l.", "liter", "liters"], "milligram": ["mg", "mg.", "milligram", "milligrams"],
-         "milliliter": ["ml", "ml.", "milliliter", "milliliters"], "pinch": ["pinch", "pinches"],
-         "dash": ["dash", "dashes"], "touch": ["touch", "touches"], "handful": ["handful", "handfuls"],
-         "stick": ["stick", "sticks"], "clove": ["cloves", "clove"], "can": ["cans", "can"],
-         "slice": ["slice", "slices"], "scoop": ["scoop", "scoops"], "filets": ["filet", "filets"], "sprig": ["sprigs", "sprig"]}
 
 UNITS = {"cup": ["cups", "cup", "c"], "fluid_ounce": ["fl oz", "fluid ounce", "fluid ounces"],
          "gallon": ["gal", "gallon", "gallons"], "ounce": ["oz", "ounce", "ounces"],
@@ -31,8 +19,11 @@ UNITS = {"cup": ["cups", "cup", "c"], "fluid_ounce": ["fl oz", "fluid ounce", "f
          "dash": ["dash", "dashes"], "touch": ["touch", "touches"], "handful": ["handful", "handfuls"],
          "stick": ["stick", "sticks"], "clove": ["cloves", "clove"], "can": ["cans", "can"],
          "slice": ["slice", "slices"], "scoop": ["scoop", "scoops"],
-         "filets": ["filet", "filets"], "sprig": ["sprigs", "sprig"],
-         "box": ["box", "boxes"]}
+         "filets": ["filet", "filets"], "sprig": ["sprigs", "sprig"], "stalk": ["stalks", "stalk"]}
+
+IGNORE = ['packages', 'package', 'boxes', 'box','packets', 'packet',
+          'pieces', 'piece', 'chunk', 'jars', 'jar', 'bags', 'bag',
+          'containers', 'container', 'bricks', 'brick', 'bottles', 'bottle']
 
 
 class String:
@@ -47,10 +38,13 @@ class String:
     def rough_clean(self):
         s1 = re.sub(r'\(.*\)', '', self.s_origin.split(',')[0])
         s2 = re.sub(r'\.', '', s1).lower()
-        s3 = clean_unicode_fractions(s2)
-        self.s_clean = s3
+        s3 = clean_pack(s2)
+        s4 = clean_unicode_fractions(s3)
+        self.s_clean = s4
 
     def find_num(self):
+        if self.s_clean.split(' ')[0] == 'one-and-a-half':
+            self.s_clean = re.sub('one-and-a-half', '1 1/2', self.s_clean)
         matcher = Matcher(NLP.vocab)
         pattern = [{'POS': 'X', 'OP': '?'}, {'POS': 'NUM', 'OP': '*'}]
         matcher.add('num', None, pattern)
@@ -59,41 +53,38 @@ class String:
         n = sorted([doc[a:b].text for _, a, b in m], key=lambda z: len(z))
         if n != []:
             self.s_clean = re.sub(n[-1], '', self.s_clean)
-            try:
-                k = float(sum(Fraction(k) for k in n[-1].split()))
-            except:
-                try:
-                    k = w2n.word_to_num(n[-1])
-                except:
-                    k = n[-1]
-                    self.check = True
-            self.num = k
+            clean_num(n[-1], self)
 
     def find_quantifier(self):
+        if self.quantifier is not None:
+            return
         words = self.s_clean.split(' ')
         for word in words:
-            for unit, lst in UNITS.items():
-                if word in lst:
-                    self.quantifier = unit
-                    self.s_clean = re.sub(word, '', self.s_clean)
-                    return
+            q = clean_quantifier(word)
+            if q is not None:
+                self.quantifier = q
+                self.s_clean = re.sub(q, '', self.s_clean)
+                return
 
     def find_root(self):
         remaining_words = self.s_clean.split()
         if len(remaining_words) == 1:
             self.root = remaining_words[0]
         else:
-            self.root = clean_verb(self.s_clean)
+            self.root = unique_words(clean_verb(self.s_clean))
 
     def check_status(self):
         if self.root is None:
             self.check = true
             return
-        elif len(self.root.split()) > 3:
+        elif len(self.root.split()) > 4:
             self.check = True
             return
         elif len(self.root.split()) > 1:
             if self.root.find('-') == 0:
+                self.check = True
+                return
+            elif self.root.find(r'\d') >= 0:
                 self.check = True
                 return
         if self.num is None or self.num == 0:
@@ -101,6 +92,17 @@ class String:
                 self.check = True
                 return
 
+
+def clean_pack(s):
+    for word in IGNORE:
+        s = s.replace(word, '')
+    return s
+
+
+def unique_words(s):
+    ulist = []
+    [ulist.append(x) for x in s.split(' ') if x not in ulist]
+    return ' '.join(ulist)
 
 
 def clean_unicode_fractions(s):
@@ -121,6 +123,53 @@ def clean_unicode_fractions(s):
 
     return s
 
+def clean_quantifier(w):
+    for unit, lst in UNITS.items():
+        if w in lst:
+            return unit
+    return None
+
+
+def clean_num(s, obj=None):
+    try:
+        k = round(float(sum(Fraction(k) for k in s.split())),2)
+        obj.num = k
+    except:
+        pass
+
+    try:
+        k = w2n.word_to_num(s)
+        obj.num = k
+    except:
+        pass
+
+    if s.find('-') >= 0:
+        words = s.split(' ')
+        if words[0] == 'half':
+            words.pop(0)
+        if len(words) == 1:
+            lst = s.split('-')
+            q = clean_quantifier(lst[-1])
+            obj.num = round(float(Fraction(lst[0])),2)
+            obj.quantifier = q
+            obj.s_clean = re.sub(lst[-1], '', obj.s_clean)
+        else:
+            temp = words.pop(0)
+            try:
+                n = int(temp)
+            except:
+                n = w2n.word_to_num(temp)
+            s_new = ' '.join(words)
+            lst = s_new.split('-')
+            n_new = round(float(sum(Fraction(t) for t in lst[0].split())),2)
+            q = clean_quantifier(lst[-1])
+            obj.quantifier = q
+            obj.s_clean = re.sub(lst[-1], '', obj.s_clean)
+            if n_new != 0:
+                obj.num = n*n_new
+            else:
+                obj.num = n
+
 
 def clean_verb(s):
     result = []
@@ -131,7 +180,7 @@ def clean_verb(s):
             w = doc[0]
             p = w.pos_
             if p == 'VERB' or p == 'ADJ' or p == 'PUNCT' \
-            or w.like_num or p == 'ADV' or w.is_stop:
+            or w.like_num or p == 'ADV' or w.is_stop or len(w.text) == 1:
                 continue
         result.append(word)
     return ' '.join(result).strip(' ')
@@ -157,4 +206,5 @@ if __name__ == "__main__":
             obj.find_quantifier()
             obj.find_root()
             obj.check_status()
-            writer.writerow([obj.num, obj.quantifier, obj.root, obj.check])
+            writer.writerow([obj.s_origin, obj.num, obj.quantifier,
+                             obj.root, obj.check])
