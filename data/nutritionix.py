15bba3b18ecdebd9af1482a489175945d2b58024
kaixinwang <kaixinwang@uchicago.edu>
2020-02-17 22:47:43 -0600

    Fetch some ingredient data

--- data/nutritionix.py
+++ data/nutritionix.py
@@ -6,25 +6,20 @@ Reference: https://github.com/leetrout/python-nutritionix
 '''
 
 import requests
+import pandas as pd
+import numpy as np
+import util
 
 URL = "https://trackapi.nutritionix.com/v2/natural/nutrients"
 
-APP_DICT = {'95dfa943': '59e2efc1a67da04885a07f8ea16d5114',
-            'd4366d31': '77c1490af84be97e73a248a2b7bb61ac',
-            '8b754284': '6cfc07a8ecd4bc61a04729e80d4170f3',
-            'c96b185b': '1ef46d0ffd3eb437b3938139995538db',
-            'ceb942f3': 'f8611c9451cda2053e7319b3ae5fe198'}
-
-USER_ID = '0'
-HEADERS = {
-    "Content-Type": "application/json",
-    "x-app-id": APP_ID,
-    "x-app-key": APP_KEY,
-    "x-remote-user-id": USER_ID
-}
+ID = [['95dfa943', '59e2efc1a67da04885a07f8ea16d5114'],
+      ['d4366d31', '77c1490af84be97e73a248a2b7bb61ac'],
+      ['8b754284', '6cfc07a8ecd4bc61a04729e80d4170f3'],
+      ['c96b185b', '1ef46d0ffd3eb437b3938139995538db'],
+      ['ceb942f3', 'f8611c9451cda2053e7319b3ae5fe198']]
 
 
-def get_nutrient(ingredient):
+def get_nutrient(ingredient, headers):
     '''
     Get nutrient informatino of an ingredient
 
@@ -35,5 +30,21 @@ def get_nutrient(ingredient):
         dictionary
     '''
     data = '{"query": "' + ingredient + '", "locale": "en_US"}'
-    response = requests.post(URL, headers=HEADERS, data=data)
+    response = requests.post(URL, headers=headers, data=data)
     return response.json()
+
+
+def get_nutrient_df(index):
+    filename = "ingredient_{}.csv".format(str(index))
+    ing_lst = pd.read_csv(filename).key.to_list()
+    appid, key = ID[index % 5]
+    headers = {"Content-Type": "application/json",
+               "x-app-id": appid,
+               "x-app-key": key,
+               "x-remote-user-id": '0'}
+    ing_dict = dict()
+    for ing in ing_lst:
+        ing_dict[ing] = get_nutrient(ing, headers)
+
+    output = str(index) + ".json"
+    util.write_json(ing_dict, output)
