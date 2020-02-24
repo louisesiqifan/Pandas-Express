6f06a74db96d5b81b4aa22a709f40f06dc1f8673
Louise Fan <louisesiqifan@gmail.com>
2020-02-23 23:16:10 -0600

    edited score_assignment

--- pandas_express/score_assignment.py
+++ pandas_express/score_assignment.py
@@ -4,6 +4,7 @@ import re
 import numpy as np
 import pandas as pd
 import configparser
+from itertools import takewhile
 from manage_input import input_verification
 
 
@@ -11,30 +12,64 @@ config = configparser.ConfigParser()
 config.read('../wrapper/constants.ini')
 DATABASE_FILENAME = config['DEFAULT']['DATABASE_FILENAME']
 INDEX_IGNORE = config['DATA']['INDEX_IGNORE']
-NUTRITION_CUT = config['SCORE']['SCORES']
+NUTRITION_CUT = {'calories': [1.10, 1.25, 1.39],
+                 'total_fat':[0.89, 5.06, 13.94],
+                 'saturated_fat': [0.16, 1.29, 5.02],
+                 'cholesterol': [0.1, 2.45, 50.25],
+                 'sodium':[20.41, 121.44, 365.03],
+                 'total_carbohydrate': [3.96, 14.88, 34.31],
+                 'dietary_fiber': [0.26, 1.22, 2.98],
+                 'sugars': [0.58,2.71,8.68],
+                 'protein': [1.68, 6.13, 16.43],
+                 'potassium': [85.53, 252.0, 463.24]}
+
+
+def get_one_dish(c, id):
+    query = f'''
+            SELECT *
+            FROM recipes
+            WHERE id = {id}
+            '''
+    c.execute(query)
+    result = c.fetchall()[0]
+    return result
+
+
+def get_default_sort(ui_input):
+    sort_list = ['score']
+    order = [False]
+    nut = ui_input.get('nutrition', None)
+    if nut is not None:
+        for k, v in nut.items():
+            sort_list.append(k)
+            if v == 1:
+                order.append(False)
+            else:
+                order.append(True)
+    sort_list.append('level')
+    order.append(True)
+    return sort_list, order
 
 
 def get_dishes(ui_input, lim=10, weight={}):
     score_ranking = score(ui_input, lim, weight)
-    ids = tuple([item[0] for item in score_ranking])
-    scores = [item[1] for item in score_ranking]
+    dishes = []
+    scores = []
     db = sqlite3.connect(DATABASE_FILENAME)
     c = db.cursor()
-    sub_q = 'or'.join([' id = ? ']*len(ids))
-    query = f'''
-            SELECT *
-            FROM recipes
-            WHERE {sub_q}
-            '''
-    c.execute(query, ids)
-    results = c.fetchall()
+    for id, s in score_ranking:
+        dish = get_one_dish(c, id)
+        dishes.append(dish)
+        scores.append(s)
     db.close()
-    columns = ['id', 'name', 'level',
-               'time_active', 'time_total', 'serving_size',
-               'directions']
-    df = pd.DataFrame(results, columns=columns)
+    columns = ['id', 'name', 'level', 'time_active', 'time_total',
+               'serving_size', 'calories', 'total_fat', 'saturated_fat',
+               'cholesterol', 'sodium', 'total_carbohydrate',
+               'dietary_fiber', 'sugars', 'protein', 'potassium', 'directions']
+    df = pd.DataFrame(dishes, columns=columns)
     df['score'] = scores
-    return df
+    sort_list, order = get_default_sort(ui_input)
+    return df.sort_values(sort_list, ascending=order)[:lim]
 
 
 def score(ui_input, lim, weight):
@@ -81,10 +116,13 @@ def score(ui_input, lim, weight):
     if nutrition:
         result = search_by_nutrition(nutrition)
         for i, val in result:
-            s = score.get(i, 0) + val*weight.get('nutrition',2)
+            s = score.get(i, 0) + val*weight.get('nutrition',1)
             score[i] = s
-    final_score = sorted(score.items(), key=lambda item: item[1], reverse=True)
-    return final_score[:lim]
+    final_score = sorted(score.items(),
+                         key=lambda item: item[1], reverse=True)
+    last = final_score[lim][1]
+    result = takewhile(lambda x: x[1]>=last, final_score)
+    return result
 
 
 def search_by_categories(args):
@@ -174,7 +212,7 @@ def search_by_title(args):
     to match recipe with input string terms
 
     Input:
-        string(list)
+        args(list)
     Return:
         results(list): list of tuple with (id, score)
     '''
@@ -239,7 +277,7 @@ def search_by_nutrition(arg_dict):
     l = len(arg_dict.keys())
     for item, side in arg_dict.items():
         cuts = NUTRITION_CUT[item]
-        args = [item, cuts[2], side, item, cuts[0], 0-side]
+        arg = [item, cuts[2], side, item, cuts[0], 0-side]
         args = args + arg
     case = '''
     CASE
@@ -262,8 +300,6 @@ def search_by_nutrition(arg_dict):
 
 
 ###########
-EXAMPLE_0 = {'categories': ['italian', 'main dish'], 'level': 'easy',
-             'time': (10, 'total'), 'title': 'fried chicken'}
-
-EXAMPLE_1 = {'categories': ['italian', 'main dish'], 'level': 'eAsy',
-             'time': (60, 'total'), 'term': 'italian soup'}
+EXAMPLE_0 = {'level': 'easy',
+             'time': (30, 'total'), 'title': 'fried chicken',
+             'nutrition': {'calories': -1, 'protein': 1}}
