a95914cdebaf8e88d5e81fe1c6df61f8fc66ff6c
jiweic <cjwbobby@gmail.com>
2020-03-01 11:14:56 -0600

    analyze data done

--- data/analyze_data.py
+++ data/analyze_data.py
@@ -10,73 +10,77 @@ import configparser
 NUTRITION_LIST = ['calories', 'total_fat', 'saturated_fat', 'cholesterol', 'sodium', 'total_carbohydrate', 'dietary_fiber', 'sugars', 'protein', 'potassium']
 config = configparser.ConfigParser()
 config.read('../wrapper/constants.ini')
-name_db = config['DATA']['NAME_DB']
-
-result_list = []
+NAME_DB = config['DATA']['NAME_DB']
 
 
-def produce_args(nutrition, percentage):
-    '''
-    Produce the args
+# def produce_args(nutrition, percentage):
+#     '''
+#     Produce the args
 
-    Inputs:
-    nutrition: String
-    percentage: Float
-    '''
-    args = ()
-    args = (str(percentage), nutrition, nutrition, nutrition, nutrition)
+#     Inputs:
+#     nutrition: String
+#     percentage: Float
+#     '''
+#     args = ()
+#     args = (percentage, nutrition, nutrition, nutrition)
 
-    return args
+#     return args
 
-def sql_to_pandas():
+def sql_to_pandas(dbfile='../data/food_map.db'):
     '''
     Convert SQL to Pandas Dataframe
     '''
-    con = sqlite3.connect('../data/food_map.db')
+    con = sqlite3.connect(dbfile)
     myFrames = pd.read_sql_query("SELECT * FROM recipes", con)
 
     return myFrames
 
-def get_33th_quantile (NUTRITION_LIST):
+
+#%%
+def get_quantile(nutrition, quantile, c):
+
+    '''
+    Helper function to get the quantile for given single nutrition input
+
+    Inputs:
+        nutrition: string
+        quantile: float
+        c: cursor 
+    Outputs:
+        result: float
+    '''
+    # args = produce_args(nutrition, quantile)       
+    args = (quantile, )
+    sql_get_quantile = config['DATA']['SQL_Get_Quantile'].format(nutrition)
+    c.execute(sql_get_quantile, args)
+    result = c.fetchall()
+    return result[0][0]
+
+
+def get_all_quantile(nut_list=NUTRITION_LIST, quantile_list=[0.33, 0.67]):
     '''
     Get the 33th quantile value and 66th quantile value for each of the nutrition factor in 
     receipe database.
+
+    Inputs:
+        nut_list: list of strings
+        quantile_list: list of floats
+
+    Outputs:
+        dictionary: key: nutrition, value: list of quantile.
     '''
+    db = sqlite3.connect(NAME_DB)
+    c = db.cursor()
+    result = dict()
+    for nutrition in nut_list:
+        quantiles = []
+        for quantile in quantile_list:
+            quantiles.append(get_quantile(nutrition, quantile, c))
+        result[nutrition] = quantiles
 
-    for nutrition in NUTRITION_LIST:
-        args = produce_args(nutrition, 0.33)       
-        sql_get_quantile = config['DATA']['SQL_Get_Quantile']
-        db = sqlite3.connect(name_db)
-        c = db.cursor()
-        print(args)
-        print(sql_get_quantile)
-        c.execute(sql_get_quantile, args)
-        results = c.fetchall()
-        print(results)
-        result_list.append(results)
-    
-    return result_list
+    db.close()
+    return result
 
 
-#def get_66th_quantile (NUTRITION_LIST):
-    '''
-    Get the 33th quantile value and 66th quantile value for each of the nutrition factor in 
-    receipe database.
-    '''
- #   for nutrition in NUTRITION_LIST:
-  #      args = produce_args(nutrition, 0.66)       
-   #     sql_get_quantile = config['DATA']['SQL_Get_Quantile']
-    #    # Connect to db
-     #   c = db.cursor()
-      #  c.execute(sql_get_quantile, args)
-       # db = sqlite3.connect(name_db)
-        #c = db.cursor()
-       # results = c.fetchall()
-        #result_list.append(results)
-    
-    return result_list
-
-# %%
-get_33th_quantile(NUTRITION_LIST)
 
 # %%
